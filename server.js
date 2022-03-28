0d9d1067b26d66b25f81e1782748246b5372e84f
Zer0cheros <christian.wiss@optimaedu.fi>
2022-03-28 12:18:05 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const expressLayout = require('express-ejs-layouts')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+const server = express()
+const indexRouter = require('./controller/index')
+
+
+
+//view engine
+server.set('view engine', 'ejs')
+server.set('layout', 'layouts/layout')
+server.use(expressLayout)
+//server.use(cookieParser)
+server.use(express.static('public'))
+server.use('/', indexRouter)
+
+server.listen(process.env.PORT, ()=>{
+    console.log('Connected');
+})
