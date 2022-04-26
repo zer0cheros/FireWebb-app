4a727b8d67bc33519efe84cb64ea8a27cd71182f
Zer0cheros <christian.wiss@optimaedu.fi>
2022-04-26 12:12:19 +0300

    update

--- controller/index.js
+++ controller/index.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const router = express.Router()
-const {index, register, login, createUser} = require('../config/router')
+const {index, register, login, createUser, profile, profileInfo, signOut} = require('../config/router')
 const {auth, db} = require('../config/FBconfig')
+const decodeIDToken = require('../config/auth')
 
-router.use('/', (req, res, next)=>{
-    console.log('check')
+router.use((req,res,next)=>{ 
     next()
 })
 
-router.get('/', index)
+router.get('/', index)   
 router.get('/register', register)
 router.get('/login', login)
 router.post('/register', createUser)
-  
+router.get('/profile', decodeIDToken, profile)
+router.post('/profile/save', decodeIDToken, profileInfo)
+router.get('/signout', signOut)
 module.exports = router
