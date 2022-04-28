f87b4da11d0791479a1ed8938f0669fe85d7e256
Zer0cheros <christian.wiss@optimaedu.fi>
2022-04-28 15:20:16 +0300

    securing app

---
+++ config/session.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const sessionConfig = {
+    secret: process.env.SECRET,
+    name: 'FireWebb-App',
+    resave: false,
+    saveUninitialized: false,
+    cookie : {
+      sameSite: 'strict',
+      httpOnly: true,
+      maxAge: 1000*60*30
+    }
+}
+
+module.exports = sessionConfig
+  
